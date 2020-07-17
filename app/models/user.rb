a0148034664b630151affd0a32f01ae17ee6c7c5
daiya1103 <doraemonyeah666@gmail.com>
2020-07-17 19:22:04 +0900

    profile-page

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,28 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_one :profile, dependent: :destroy
+
+  delegate :age, :birthday, :gender, to: :profile, allow_nil: true
   
   def has_written?(article)
     articles.exists?(id: article.id)
+  end
+
+  def prepare_profile
+    profile || build_profile
+  end
+
+  def disp_name
+    # ぼっち演算子
+    profile&.nickname || self.email.split('@').first
+  end
+
+  def avatar_image
+    if profile&.avatar&.attached?
+      profile.avatar
+    else
+      'default-avatar.png'
+    end
   end
 end
